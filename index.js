85d941b4c1c0a8958b226c48d0db89b7872ab1bc
emilemuny <emilemuny007@gmail.com>
2015-02-16 10:37:45 -0800

    adding changes/now connected tothe db

--- index.js
+++ index.js
@@ -1,19 +1,41 @@
+//
+// var Twitter = require('twitter');
+//
+// var client = new Twitter({
+//   consumer_key: process.env.TWITTER_CONSUMER_KEY,
+//   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+//   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+//   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+// });
+//
+// client.stream('statuses/filter', {track: 'coding house,codinghouse'}, function(stream) {
+//   stream.on('data', function(tweet) {
+//     console.log(tweet.text);
+//   });
+//
+//   stream.on('error', function(error) {
+//     throw error;
+//   });
+// });
 
-var Twitter = require('twitter');
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
 
-var client = new Twitter({
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
-});
+// Connection URL
+var url = 'mongodb://localhost:27017/myproject';
+// Use connect method to connect to the Server
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server");
+  //Get the documents collection
 
-client.stream('statuses/filter', {track: 'coding house,codinghouse'}, function(stream) {
-  stream.on('data', function(tweet) {
-    console.log(tweet.text);
-  });
+    var collection = db.collection('documents');
+    // Insert some documents
+    collection.insert(
+      {b : 1, c: 2}
+    , function(err, result) {
+      assert.equal(err, null);
+      db.close();
+    });
 
-  stream.on('error', function(error) {
-    throw error;
-  });
 });
